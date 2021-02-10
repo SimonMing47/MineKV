2c14c0357830f090e9c9176b113507ca658ed8aa
simon <mingxing_47@126.com>
2021-02-10 13:57:39 +0800

    init project

---
+++ minekv/target/rls/debug/deps/minekv-1a5694ceba929ff5.d
@@ -0,0 +1,5 @@
+/Users/simonming/Dev/Projects/MineKV/minekv/target/rls/debug/deps/minekv-1a5694ceba929ff5.rmeta: src/main.rs
+
+/Users/simonming/Dev/Projects/MineKV/minekv/target/rls/debug/deps/minekv-1a5694ceba929ff5.d: src/main.rs
+
+src/main.rs:
---
+++ minekv/target/rls/debug/deps/minekv-66f45af5e081288b.d
@@ -0,0 +1,5 @@
+/Users/simonming/Dev/Projects/MineKV/minekv/target/rls/debug/deps/minekv-66f45af5e081288b.rmeta: src/main.rs
+
+/Users/simonming/Dev/Projects/MineKV/minekv/target/rls/debug/deps/minekv-66f45af5e081288b.d: src/main.rs
+
+src/main.rs:
